4e5ab9fc4e2d3f3900a6dc87bd7695a06110fa76
dvino <dvino@sers.noreply.github.com>
2022-06-02 10:30:29 +0000

    generated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
 	*/
 	var now = new Date();
 	var countTo = 25 * 24 * 60 * 60 * 1000 + now.valueOf();    
-	$('.timer').countdown("2022/07/21", function(event) {
+	$('.timer').countdown("2022/08/08", function(event) {
 		$(this).find('.days').text(event.offset.totalDays);
 		$(this).find('.hours').text(event.offset.hours);
 		$(this).find('.minutes').text(event.offset.minutes);
